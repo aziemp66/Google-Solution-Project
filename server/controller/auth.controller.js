49336f584e011b7eab6aecd0287f888e992db4a4
Azie Melza Pratama <aziemp66@gmail.com>
2022-03-19 13:26:22 +0700

    Creating Investor Register

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,10 +1,26 @@
 const Investor = require("../model/investor.model");
+const validation = require("../util/validation");
+const bcrypt = require("bcryptjs");
 
 async function investorRegister(req, res) {
+	//Validating Data
+	const { error } = validation.registerValidation(req.body);
+	if (error) return res.status(400).json({ error: error.details[0].message });
+
+	//Checking if user already exists
+	const userFound = await Investor.findOne({ email: req.body.email });
+	if (userFound)
+		return res.status(400).json({ error: "User already exists" });
+
+	//Hashing Password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	//Creating new user
 	const investor = new Investor({
 		name: req.body.name,
 		email: req.body.email,
-		password: req.body.password,
+		password: hashedPassword,
 	});
 	try {
 		await investor.save();
---
+++ server/util/validation.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+
+//Register Validation
+const registerValidation = (data) => {
+	const schema = Joi.object({
+		name: Joi.string().min(6).max(255).required(),
+		email: Joi.string().min(6).max(255).required().email(),
+		password: Joi.string().min(6).max(1024).required(),
+	});
+
+	return schema.validate(data);
+};
+
+const loginValidation = (data) => {
+	const schema = Joi.object({
+		email: Joi.string().min(6).max(255).required().email(),
+		password: Joi.string().min(6).max(1024).required(),
+	});
+
+	return schema.validate(data);
+};
+
+module.exports = { registerValidation, loginValidation };
