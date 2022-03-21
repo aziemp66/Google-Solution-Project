11e023b8f1e3eaa6b840993020d32d18d36aff8d
zidane-arkan <zidark897@Gmail.com>
2022-03-21 21:48:03 +0700

    Add Folder Search, Login, Investor

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './index.css';
-
+import Search from "./Components/Search/Search" ;
 
 function App() {
   return (
     <div className="App">
-    
+        <Search></Search>
     </div>
   );
 }
---
+++ client/src/Components/Search/Search.js
@@ -0,0 +1,9 @@
+function Search() { 
+    return (
+        <div>
+            <p>Nama</p>
+        </div>
+    );
+}
+
+export default Search;
