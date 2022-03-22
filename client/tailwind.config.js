c4cdfbfd5943dc207cdfb0848258d569f3e97a1c
Muhammad Rizki <Zenkriztao3@gmail.com>
2022-03-23 03:32:34 +0700

    chore: fix extend obj spacing style

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+      '0.1': '0.063rem'
+      },
+    },
   },
   plugins: [],
 };
