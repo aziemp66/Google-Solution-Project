eef0df20a073aeef3b08804a2e4e74b78ed122f5
Muhammad Rizki <Zenkriztao3@gmail.com>
2022-03-23 04:06:18 +0700

    fix typo

--- client/src/Components/Section.js
+++ client/src/Components/Section.js
@@ -4,9 +4,9 @@ import Fade from 'react-reveal/Fade';
 const Section = () => { 
  return (
         <section className="w-full h-screen">
-            <Fade Top>
-                <img className="object-cover w-full h-3/4 " src="../../../assets/heroSection.png" alt="banner" />
-           </Fade>
+             <Fade top>
+                 <img className="object-cover w-full h-3/4 " src="../../../assets/heroSection.png" alt="banner" />
+             </Fade>
         </section>
     )
 }
