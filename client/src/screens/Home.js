e27a06e0c6c1b029ee1eea658269bda379d2e5fc
Muhammad Rizki <Zenkriztao3@gmail.com>
2022-03-23 03:41:42 +0700

    feat: add pages screen folder (#1)

---
+++ client/src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Section from "../components/Section";
+import Menu from "../components/Product/Product";
+
+const Home = () => {
+    return (
+        <main className="w-full bg-[#E6EDE9]">
+            <Section/>
+            <Menu/>
+        </main>
+    );
+}
+
+export default Home;
