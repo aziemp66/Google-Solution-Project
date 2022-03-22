5d82915cbd5d621e593400191d981be9be90ffa5
Muhammad Rizki <Zenkriztao3@gmail.com>
2022-03-23 04:08:13 +0700

    refactor: fix prettier autocomplete

--- client/src/Components/Title.js
+++ client/src/Components/Title.js
@@ -6,8 +6,9 @@ const Title = ({title}) => {
         <Bounce top>
             <div className="flex flex-col items-center pb-1 space-x-2">
                 <h1 className="text-3xl text-gray-700 poppins"><span className="font-semibold select-none text-black-600">{title}</span></h1>
-                <div className="flex items-center justify-center w-16 h-1 mt-2 bg-black rounded-full"></div>
-            </div>
+                   <div className="flex items-center justify-center w-16 h-1 mt-2 bg-black rounded-full">
+                </div>
+              </div>
         </Bounce>
     )
 }
