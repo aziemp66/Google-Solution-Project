f151072186876b41afa9ddc9eac5b3424fe07ad5
Muhammad Rizki <Zenkriztao3@gmail.com>
2022-03-23 19:14:18 +0700

    feat: add folder Article

---
+++ client/src/Components/Article/Article.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../Title";
+
+const Article = () => {
+  return (
+    <section className="max-w-screen-xl px-6 py-6 pb-24 mx-auto">
+  
+        <Title title="Article" />
+
+    </section>
+  );
+}
+
+export default Article;
