3309e2bf4b5d2da18a2ccbbd4d97f88e72c02681
zidane-arkan <zidark897@Gmail.com>
2022-03-29 11:39:14 +0700

    Build Transaction Page
    Add transaction page and make route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Search from "./components/Search/SearchPage";
 import InvestorPage from "./components/Investor/InvestorPage";
 import ProfileInvestor from "./components/ProfileInvestor/ProfileInvestor";
 import ProfileInvestment from "./components/ProfileInvestment/ProfileInvestment";
+import Transaction from "./components/Transaction/Transaction";
 
 const App = () => {
 	return (
@@ -25,6 +26,7 @@ const App = () => {
 					component={ProfileInvestment}
 				/>
 				<Route exact path="/investor" component={InvestorPage} />
+				<Route exact path="/transaction" component={Transaction} />
 			</Switch>
 		</BrowserRouter>
 	);
---
+++ client/src/components/Transaction/Transaction.js
@@ -0,0 +1,13 @@
+function Transaction() { 
+    return (
+        <section className="flex flex-col gap-12 pb-12 2xl:pb-12 px-40 2xl:px-40 w-full 2xl:w-full h-[100vh] 2xl:h-[100vh] align-middle bg-[#E6EDE9]">
+            <div className="flex flex-col w-full 2xl:w-full gap-4">
+                <div className="flex justify-start items-start w-full 2xl:w-full  mt:40 2xl:mt-40 mb-0 2xl:mb-0">
+                    <h1 className="font-bold text-[#008C41] text-xl 2xl:text-3xl" >INVESTMENT TRANSACTION</h1>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Transaction;
